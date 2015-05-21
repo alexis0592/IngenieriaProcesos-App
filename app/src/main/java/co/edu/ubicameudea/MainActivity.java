fc17933a3d485be7090ba946b741b664a9af05e4
Yefry Calderón <yefry.calderon@udea.edu.co>
2015-05-21 10:00:49 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/ubicameudea/MainActivity.java
@@ -0,0 +1,56 @@
+package co.edu.ubicameudea;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import co.edu.ubicameudea.activities.UdeaMapActivity;
+
+import co.edu.ubicameudea.activities.UdeaMapActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ImageView imgMapButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    public void initComponents(){
+        this.imgMapButton = (ImageView)super.findViewById(R.id.goMapButton);
+    }
+
+    public void viewMapClick(View view){
+        UdeaMapActivity mapActivity = new UdeaMapActivity();
+        startActivity(new Intent(this, mapActivity.getClass()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/co/edu/ubicameudea/activities/UdeaMapActivity.java
@@ -0,0 +1,54 @@
+package co.edu.ubicameudea.activities;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
+import co.edu.ubicameudea.R;
+
+public class UdeaMapActivity extends Activity implements OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_udea_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_udea_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-18.142, 178.431), 2));
+
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+}
